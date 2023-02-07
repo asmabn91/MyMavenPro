9d6470169fea6fd7c7291fde20f5765610fa1cb2
asma <asma@portakal.net>
2023-02-06 18:58:58 -0800

    My First Maven Project

---
+++ src/test/java/TechnostudyB7_Day1/SeleniumTest.java
@@ -0,0 +1,30 @@
+package TechnostudyB7_Day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+//2- create object of ChromeDriver to open the browser
+        WebDriver driver= new ChromeDriver();
+//3- go to google page
+        driver.get("https://www.google.com");
+        driver.getTitle();
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().to("https://campus.techno.study");
+        String expectedTitle="Join the best coding bootcamp online and get IT training in the US";
+        String actualTitle=driver.getTitle();
+        if(expectedTitle.equals(actualTitle)){
+            System.out.printf("verification is done: Pass");
+            System.out.printf("Actual Title = "+actualTitle);
+        }else {
+            System.out.println("verification is Failed");
+            System.out.println("actual Title = "+actualTitle);
+        }
+
+
+    }
+}
