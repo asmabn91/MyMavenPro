e566fc2fe62ff8749ad2833b306e844730d46140
asma <asma@portakal.net>
2023-02-17 18:55:35 -0800

    Day7

---
+++ src/test/java/TechnostudyB7/Day7/ActionsClickAndHold.java
@@ -0,0 +1,31 @@
+package TechnostudyB7.Day7;
+
+import TechnostudyB7.Utilities.Utility1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsClickAndHold  extends Utility1 {
+    public static void main(String[] args) {
+        driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+
+        //<div class="dragableBox" id="box3" dragableelement="2"
+        // style="visibility: visible;">Washington</div>
+        WebElement washington = driver.findElement(By.id("box3"));
+
+        WebElement unitedStates = driver.findElement(By.id("box103"));
+
+        Actions actions = new Actions(driver);
+
+//        Action clickAndHoldWashington = actions.clickAndHold(washington).build();
+//        clickAndHoldWashington.perform();
+//
+//        Action moveWashington = actions.moveToElement(unitedStates).release().build();
+//        moveWashington.perform();
+
+        Action clickAndHoldWashington2 = actions.clickAndHold(washington).moveToElement(unitedStates).release().build();// combined 2 actions
+        clickAndHoldWashington2.perform();
+    }
+
+}
